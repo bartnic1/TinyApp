2b29a294fb45bb5e9f13295687a9c245d612c560
Piotr <bartnic1@gmail.com>
2018-02-20 19:59:40 +0000

    Created basic express server, and urls.ejs files which show placeholder URLs

---
+++ express_server.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+//Remember: This will create a server at http://localhost:8080/urls
+//Additional paths may be specified after the port number
+
+
+//When you pass an object into res.render, it gives you access to all the key-value
+//pairs inside that object. Thus you can call entries["b2xVn2"], and you can loop over them:
+/*
+for(var index in entries){
+  console.log(index) //Will print b2xvn2...
+  console.log(entries[index]) //Will print www.lighthouselabs.ca ...
+}
+*/
+var urlDatabase = {
+  entries: {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+  }
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+// http://localhost:8080/urls
+app.get("/urls", (req, res) => {
+  let templateVars = urlDatabase;
+  res.render("urls_index", templateVars)
+});
+
+// http://localhost:8080/urls/b2xVn2
+// http://localhost:8080/urls/9sm5xK
+app.get("/urls/:id", (req, res) => {
+  let singleEntry = {entry: {"short": req.params.id, "long": urlDatabase.entries[req.params.id]}};
+  // var singleEntry = {entry: {req.params.id: urlDatabase.entries[req.params.id]}};
+  let templateVars = singleEntry;
+  res.render("urls_show", templateVars);
+});
+
+/*
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+*/
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
