d77b2d892c32f84070e5c6a2571fb8bdb95b4d6d
Piotr <bartnic1@gmail.com>
2018-02-23 07:13:36 +0000

    Changed pictures

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+//Import all critical dependencies
+
 const cookieSession = require('cookie-session');
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -5,6 +7,8 @@ const bcrypt = require('bcrypt');
 
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
+
+//Set up the middleware and the embedded javascript view engine
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,26 +18,22 @@ app.use(cookieSession({
   maxAge: 24*60*60*1000
 }))
 
-
-//Remember: This will create a server at http://localhost:8080/urls
-//Additional paths may be specified after the port number
-
-//Not using this anymore, but keep as backup:
-//const cookieParser = require('cookie-parser');
-//app.use(cookieParser());
-
+//This function generates the tiny URL by randomly choosing values from an
+//alphanumeric string.
 function generateRandomString(){
   function randomInt36(){
     return Math.floor(Math.random()*36);
   }
   let newURL = '';
-  let alphanumVals = 'abcdefghijklmnopqrstuvwxyz0123456789'; //36 vals
+  let alphanumVals = 'abcdefghijklmnopqrstuvwxyz0123456789';
   for(let i = 0; i < 6; i++){
     newURL += alphanumVals[randomInt36()]
   }
   return newURL;
 }
 
+//This function checks which URLs a user has access to, and logs them to a local
+//URL database (with global scope).
 function urlsForUser(userID){
   //Clear the database of any previous user data
   delete localUrlDatabase.entries;
@@ -46,14 +46,7 @@ function urlsForUser(userID){
   }
 }
 
-//When you pass an object into res.render, it gives you access to all the key-value
-//pairs inside that object. Thus you can call entries["b2xVn2"], and you can loop over them:
-/*
-for(var index in entries){
-  console.log(index) //Will print b2xvn2...
-  console.log(entries[index]) //Will print www.lighthouselabs.ca ...
-}
-*/
+//This object defines all of the registered users. Some default users exist as examples.
 const users = {
   "userRandomID": {
     id: "userRandomID",
@@ -67,6 +60,10 @@ const users = {
   }
 }
 
+//The urlDatabase contains all of the URLs created by all users. The localUrlDatabase
+//only contains those Urls that have been created by their respective users.
+
+//Each shortURL is assigned a userID for easy identification.
 const urlDatabase = {
   entries: {
   "b2xVn2": {userID: "userRandomID", url: "http://www.lighthouselabs.ca"},
@@ -78,7 +75,10 @@ const localUrlDatabase = {
   entries: {}
 };
 
-//redirect is better than render, since the latter has to recreate the entire page!
+//The call to post to the login screen is used to enter e-mail and password data.
+//If everything matches, a cookie is created based on the user's ID (from the database above)
+
+//Passwords use bcrypt hashes to protect users from server break-ins.
 app.post("/login", (req, res) => {
   let emailFound = false;
   let passFound = false;
@@ -102,11 +102,13 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+//Clears the cookies from the server and redirects to the main URLs page.
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
+//Generates a tinyURL based on values entered into a form.
 app.post("/urls", (req, res) => {
   if(Object.keys(req.session).length === 0){
     return res.status(401).send("No registered user detected. Access denied.")
@@ -116,6 +118,8 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${randURL}`);
 });
 
+//Registers the user in the users database, and automatically logs in the user by creating
+//a session cookie. Passwords are once again stored in hashed form for extra protection.
 app.post("/register", (req, res) => {
   let newID = generateRandomString();
   let userEmail = req.body.email;
@@ -134,6 +138,7 @@ app.post("/register", (req, res) => {
   res.redirect("/urls");
 })
 
+//Allows a registered user to delete his/her own unwanted URLs.
 app.post("/urls/:id/delete", (req, res) => {
   if(Object.keys(req.session).length === 0){
     return res.status(401).send("No registered user detected. Access denied.")
@@ -145,6 +150,7 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+//Allows users to edit their long-form URL, if desired.
 app.post("/urls/:id", (req, res) => {
   if(Object.keys(req.session).length === 0){
     return res.status(401).send("No registered user detected. Access denied.")
@@ -156,6 +162,8 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+//The root directory serves no real function in this app, and so redirects to
+//appropriate directories depending on whether a user is logged in or not.
 app.get("/", (req, res) => {
   if(Object.keys(req.session).length === 1){
     res.redirect("/urls");
@@ -164,7 +172,7 @@ app.get("/", (req, res) => {
   }
 });
 
-// http://localhost:8080/urls/new
+//Allows a user to visit a page where they may create a new tinyURL for themselves.
 app.get("/urls/new", (req, res) => {
   if(Object.keys(req.session).length === 0){
     return res.redirect("/login");
@@ -172,7 +180,8 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", {urlDatabase: urlDatabase, user: users[req.session.user_id]});
 });
 
-// http://localhost:8080/urls
+//This is the main page, that displays all of a user's personal tinyURLs. The urlsForUser()
+//function call ensurse that only URLs tied to their account are visible.
 app.get("/urls", (req, res) => {
   let userInfo;
   for(var user in users){
@@ -181,13 +190,10 @@ app.get("/urls", (req, res) => {
     }
   }
   urlsForUser(req.session.user_id);
-  //Note that by default, if userInfo is undefined, then entering "user" into
-  //urls_index.ejs will result in nothing being generated, because it automatically
-  //outputs the value of the key "user".
-
   res.render("urls_index", {urlDatabase: localUrlDatabase, user: userInfo});
 });
 
+//This displays the registration page, where a user may register a new account.
 app.get("/register", (req, res) => {
   if(Object.keys(req.session).length === 1){
     return res.redirect("/urls");
@@ -195,6 +201,7 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+//This displays the login pages, where pre-existing users may login to their accounts.
 app.get("/login", (req, res) => {
   if(Object.keys(req.session).length === 1){
     return res.redirect("/urls");
@@ -202,6 +209,8 @@ app.get("/login", (req, res) => {
   res.render("login")
 })
 
+//This displays a user's long-form URL together with its tinyURL form.
+//The user may either confirm its contents or augment it using the associated POST method to the same URL.
 app.get("/urls/:id", (req, res) => {
   //First, test whether the appropriate user has access to the resources on this page
   let accessBool = false;
@@ -221,6 +230,8 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", singleEntry);
 });
 
+//This allows the user to enter their tinyURL into the browser address bar, which will redirect them to
+//the long-form URL.
 app.get("/u/:shortURL", (req, res) => {
   if(urlDatabase.entries[req.params["shortURL"]] === undefined){
     return res.status(404).send("Invalid tinyURL code entered. Redirect aborted.")
@@ -229,8 +240,7 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-// null undefined false 0 [] ''
-
+//In the terminal, confirms that the local server is operational.
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
