7a798e5e142091f8134a0ff507d5ed2bc1ee6332
Piotr <bartnic1@gmail.com>
2018-02-23 17:12:58 +0000

    Added a date when URLs are created

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-//Import all critical dependencies
+//Import all dependencies
 
 const cookieSession = require('cookie-session');
 const express = require("express");
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 
 //Set up the middleware and the embedded javascript view engine
-
+//CookieSession is useful for encrypting cookies
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
@@ -18,8 +18,7 @@ app.use(cookieSession({
   maxAge: 24*60*60*1000
 }))
 
-//This function generates the tiny URL by randomly choosing values from an
-//alphanumeric string.
+//This function generates the tiny URL by randomly choosing values from an alphanumeric string.
 function generateRandomString(){
   function randomInt36(){
     return Math.floor(Math.random()*36);
@@ -32,8 +31,7 @@ function generateRandomString(){
   return newURL;
 }
 
-//This function checks which URLs a user has access to, and logs them to a local
-//URL database (with global scope).
+//This function checks which URLs a user has access to, and logs them to a local URL database (with global scope).
 function urlsForUser(userID){
   //Clear the database of any previous user data
   delete localUrlDatabase.entries;
@@ -44,6 +42,20 @@ function urlsForUser(userID){
       localUrlDatabase["entries"][entry] = urlDatabase.entries[entry];
     }
   }
+}
+//Gets the current date; used when generating new URLs.
+function getDate(){
+  let today = new Date();
+  let day = today.getDate();
+  let month = today.getMonth() + 1;
+  let year = today.getFullYear();
+  if(day < 10){
+    day += '0' + day;
+  }
+  if(month < 10){
+    month = '0' + month;
+  }
+  return `${month}/${day}/${year}`
 }
 
 //This object defines all of the registered users. Some default users exist as examples.
@@ -60,14 +72,13 @@ const users = {
   }
 }
 
-//The urlDatabase contains all of the URLs created by all users. The localUrlDatabase
-//only contains those Urls that have been created by their respective users.
+//The urlDatabase contains the URLs created by all users. The localUrlDatabase dynamically allocates URLs that were created by currently logged-in users, in the same format.
 
-//Each shortURL is assigned a userID for easy identification.
+//Each shortURL is assigned a userID, a long URL, and the time it was created (month/day/year)
 const urlDatabase = {
   entries: {
-  "b2xVn2": {userID: "userRandomID", url: "http://www.lighthouselabs.ca"},
-  "9sm5xK": {userID: "user2RandomID", url: "http://www.google.com"}
+  "b2xVn2": {userID: "userRandomID", url: "http://www.lighthouselabs.ca", time:"05/09/2005" },
+  "9sm5xK": {userID: "user2RandomID", url: "http://www.google.com", time: "01/10/2009"}
   }
 };
 
@@ -75,10 +86,13 @@ const localUrlDatabase = {
   entries: {}
 };
 
+//-----------------------------------------------------------------//
+//App.post requests below
+//-----------------------------------------------------------------//
+
+
 //The call to post to the login screen is used to enter e-mail and password data.
 //If everything matches, a cookie is created based on the user's ID (from the database above)
-
-//Passwords use bcrypt hashes to protect users from server break-ins.
 app.post("/login", (req, res) => {
   let emailFound = false;
   let passFound = false;
@@ -108,18 +122,18 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
-//Generates a tinyURL based on values entered into a form.
+//Generates a tinyURL based on values entered into a form, including the date it was created.
 app.post("/urls", (req, res) => {
   if(Object.keys(req.session).length === 0){
     return res.status(401).send("No registered user detected. Access denied.")
   }
   let randURL = generateRandomString();
-  urlDatabase.entries[randURL] = {userID: req.session.user_id, url: req.body["longURL"]};
+  let date = getDate();
+  urlDatabase.entries[randURL] = {userID: req.session.user_id, url: req.body["longURL"], date: date};
   res.redirect(`/urls/${randURL}`);
 });
 
-//Registers the user in the users database, and automatically logs in the user by creating
-//a session cookie. Passwords are once again stored in hashed form for extra protection.
+//Registers the user in the users database, and automatically logs in the user by creating a session cookie. Passwords are once again stored in hashed form for extra protection.
 app.post("/register", (req, res) => {
   let newID = generateRandomString();
   let userEmail = req.body.email;
@@ -162,8 +176,11 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
-//The root directory serves no real function in this app, and so redirects to
-//appropriate directories depending on whether a user is logged in or not.
+//-----------------------------------------------------------------//
+//App.get requests below
+//-----------------------------------------------------------------//
+
+//The root directory serves no real function in this app, and so redirects to appropriate directories depending on whether a user is logged in or not.
 app.get("/", (req, res) => {
   if(Object.keys(req.session).length === 1){
     res.redirect("/urls");
@@ -180,8 +197,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", {urlDatabase: urlDatabase, user: users[req.session.user_id]});
 });
 
-//This is the main page, that displays all of a user's personal tinyURLs. The urlsForUser()
-//function call ensurse that only URLs tied to their account are visible.
+//This is the main page, that displays all of a user's personal tinyURLs. The urlsForUser() function call ensurse that only URLs tied to their account are visible.
 app.get("/urls", (req, res) => {
   let userInfo;
   for(var user in users){
@@ -190,7 +206,7 @@ app.get("/urls", (req, res) => {
     }
   }
   urlsForUser(req.session.user_id);
-  res.render("urls_index", {urlDatabase: localUrlDatabase, user: userInfo});
+  res.render("urls_index", {localUrlDatabase: localUrlDatabase, user: userInfo});
 });
 
 //This displays the registration page, where a user may register a new account.
@@ -209,8 +225,7 @@ app.get("/login", (req, res) => {
   res.render("login")
 })
 
-//This displays a user's long-form URL together with its tinyURL form.
-//The user may either confirm its contents or augment it using the associated POST method to the same URL.
+//This displays a user's long-form URL together with its tinyURL form. The user may either confirm its contents or augment it using the associated POST method to the same URL.
 app.get("/urls/:id", (req, res) => {
   //First, test whether the appropriate user has access to the resources on this page
   let accessBool = false;
@@ -230,8 +245,7 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", singleEntry);
 });
 
-//This allows the user to enter their tinyURL into the browser address bar, which will redirect them to
-//the long-form URL.
+//This allows the user to enter their tinyURL into the browser address bar, which will redirect them to the long-form URL.
 app.get("/u/:shortURL", (req, res) => {
   if(urlDatabase.entries[req.params["shortURL"]] === undefined){
     return res.status(404).send("Invalid tinyURL code entered. Redirect aborted.")
